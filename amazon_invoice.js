054d80bc7c80cc7552dcc9b5821b3c1c50910f39
jxramos <jaime.x.ramos@gmail.com>
2020-02-10 22:00:29 -0800

    populate top level order details * parsing Amazon invoice page for order number, order date,   ship date, and order total.

--- amazon_invoice.js
+++ amazon_invoice.js
@@ -1,1 +1,18 @@
 console.log("Amazon webpage")
+
+var transaction ={};
+
+// Get Order Number
+transaction["Order#"] = document.getElementsByClassName("h1")[0].innerText.replace("Final Details for ","");
+
+// Get OrderDate
+xpathOrderDate = "/html/body/table/tbody/tr/td/table[1]/tbody/tr[1]/td"
+transaction["OrderDate"] = document.evaluate(xpathOrderDate, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.innerText.split(": ")[1];
+
+// Get ShipDate
+xpathShipDate = "/html/body/table/tbody/tr/td/table[2]/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td/b/center"
+transaction["ShipDate"] = document.evaluate(xpathShipDate, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.innerText.replace("Shipped on","");
+
+// Get Order Total
+xpathOrderTotal = "/html/body/table/tbody/tr/td/table[1]/tbody/tr[3]/td/b";
+transaction["Total"] = parseFloat(document.evaluate(xpathOrderTotal, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.innerText.replace("Order Total: $",""));
