07d749e9a691c52b037f88967cd0d8e6140617ca
jxramos <jaime.x.ramos@gmail.com>
2020-02-11 21:54:46 -0800

    reformat source and get subscribe and save status * break up long commands to line separated chained commands. * evaluate if the Amazon transaction is a subscribe and save type.

--- amazon_invoice.js
+++ amazon_invoice.js
@@ -6,13 +6,22 @@ var transaction ={};
 transaction["Order#"] = document.getElementsByClassName("h1")[0].innerText.replace("Final Details for ","");
 
 // Get OrderDate
-xpathOrderDate = "/html/body/table/tbody/tr/td/table[1]/tbody/tr[1]/td"
-transaction["OrderDate"] = document.evaluate(xpathOrderDate, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.innerText.split(": ")[1];
+xpathOrderDate = "/html/body/table/tbody/tr/td/table[1]/tbody/tr[1]/td";
+dateText = document.evaluate(xpathOrderDate, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null )
+                   .singleNodeValue.innerText;
+transaction["OrderDate"] = dateText.split(": ")[1];
+
+// Get Substribe & Save Status
+transaction["IsSubscribeSave"] = dateText.includes("Subscribe and Save");
 
 // Get ShipDate
-xpathShipDate = "/html/body/table/tbody/tr/td/table[2]/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td/b/center"
-transaction["ShipDate"] = document.evaluate(xpathShipDate, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.innerText.replace("Shipped on","");
+xpathShipDate = "/html/body/table/tbody/tr/td/table[2]/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td/b/center";
+transaction["ShipDate"] = document.evaluate(xpathShipDate, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null )
+                                  .singleNodeValue.innerText
+                                  .replace("Shipped on","");
 
 // Get Order Total
 xpathOrderTotal = "/html/body/table/tbody/tr/td/table[1]/tbody/tr[3]/td/b";
-transaction["Total"] = parseFloat(document.evaluate(xpathOrderTotal, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.innerText.replace("Order Total: $",""));
+transaction["Total"] = parseFloat(document.evaluate(xpathOrderTotal, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null )
+                                  .singleNodeValue.innerText
+                                  .replace("Order Total: $",""));
