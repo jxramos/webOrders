d292dc89746c8f06954106bd137a2261db5c030f
jxramos <jaime.x.ramos@gmail.com>
2020-02-17 23:20:41 -0800

    fix order total bug in marketplace orders Order Total is no always the third entry, in the table turns out that "Seller's order number: XYZ" could land in the table which yields a parseFload of NaN. Replace xpath index 3 with last() to be more robust to this.

--- amazon_invoice.js
+++ amazon_invoice.js
@@ -55,7 +55,7 @@ function getOrderMetaData(transaction) {
     transaction["IsSubscribeSave"] = dateText.includes("Subscribe and Save");
 
     // Get Order Total
-    xpathOrderTotal = "/html/body/table/tbody/tr/td/table[1]/tbody/tr[3]/td/b";
+    xpathOrderTotal = "/html/body/table/tbody/tr/td/table[1]/tbody/tr[last()]/td/b";
     transaction["Total"] = parseFloat(document.evaluate(xpathOrderTotal, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null )
                                     .singleNodeValue.innerText
                                     .replace("Order Total: $",""));
