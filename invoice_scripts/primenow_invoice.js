a11dbbb7bc20e6bc7d5660d75fc8ec041edd1754
jxramos <jaime.x.ramos@gmail.com>
2020-04-07 23:32:37 -0700

    implement first draft of price itemization scraping * has known bug for those entries that have an Add button   present

--- invoice_scripts/primenow_invoice.js
+++ invoice_scripts/primenow_invoice.js
@@ -14,6 +14,7 @@ function scrapeOrderData(transaction) {
 
     getOrderMetaData(transaction);
     getPaymentMetaData(transaction);
+    getOrderItemization(transaction);
 }
 
 function downloadContent(filename, content) {
@@ -79,6 +80,48 @@ function getPaymentMetaData(transaction) {
 
     transaction["PaymentMethod"] = payment_metadata;
 }
+
+/*==========================================================================================
+ORDER ITEMIZATION
+==========================================================================================*/
+
+function getOrderItemization(transaction){
+    console.log("getOrderItemization");
+
+    var purchased_items = [];
+
+    // Get Items Purchased
+    xpathItemsPurchased = "/html/body/div[1]/div[1]/div[4]/div[3]/div[3]/div[2]/div/div[position() > 1]";
+    itemsPurchasedXPR = document.evaluate(xpathItemsPurchased, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+
+    // Parse purchased items
+    while ((nodePurchasedItem = itemsPurchasedXPR.iterateNext()) != null) {
+        var purchased_item = []
+
+        // Drill into purchased item
+        var xpathItem = "./div/a/div/div/div[2]"; // TODO this breaks when there's an Add button in the DOM
+        var itemXPR = document.evaluate(xpathItem, nodePurchasedItem, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        var itemNode = itemXPR.singleNodeValue.childNodes;
+        var priceNodes = itemNode[3].children;
+
+        //-------------------------
+        // Item Description
+        var quantity = priceNodes[0].innerText.split("$")[0].trim()
+        description = quantity + "; " + itemNode[1].innerText;
+        purchased_item.push(description)
+
+        //-------------------------
+        // Item Price
+        var price = parsePrice(priceNodes[1]);
+        purchased_item.push(price);
+
+        // Integrate line item
+        purchased_items.push(purchased_item);
+    }
+
+    transaction["Items"] = purchased_items;
+}
+
 function parsePrice(item){
     return parseFloat(item.textContent.trim().replace('$',''))
 }
