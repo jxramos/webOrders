d7b969cfcc00fe3315873c623ec7d1b0685f0c57
jxramos <jaime.x.ramos@gmail.com>
2025-07-31 01:35:43 -0700

    fixing extension button callback logic bugs * `replace` only changes one instance in the string * the matches inner for loop had improper syntax * the callback always matches for Amazon invoices which is what was tested on, but failed for other invoices.

--- common/service-worker.js
+++ common/service-worker.js
@@ -1,9 +1,12 @@
 chrome.action.onClicked.addListener((tab) => {
+    console.log("Downloading web order for " + tab.url)
     // Match the URL of the active tab to its corresponding content scripts
     content_script_js_files = []
     for (const content_script of chrome.runtime.getManifest().content_scripts) {
-        for (const match in content_script.matches) {
-            const re = new RegExp(match.replace("*", ".*"));
+        for (const match of content_script.matches) {
+            // transform the manifest matches glob syntax to regular expression syntax
+            match_re = match.replace(/[.?*]/g, (m) => m === '.' ? '\\.' : m === '?' ? '\\?' : '.*');
+            const re = new RegExp(match_re);
             if (re.test(tab.url)){
                 content_script_js_files = content_script.js
                 break;
