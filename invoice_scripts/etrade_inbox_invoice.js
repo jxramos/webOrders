023f4dc5fdd684f76247364541c31d262ba2ac95
jxramos <jaime.x.ramos@gmail.com>
2026-02-16 11:04:50 -0800

    implementing eTrade inbox invoice parsing

---
+++ invoice_scripts/etrade_inbox_invoice.js
@@ -0,0 +1,74 @@
+function processETradeInboxInvoice() {
+    console.log("processETradeInboxInvoice")
+
+    var transaction = {
+        "Vendor": "eTrade.com",
+        "URL": window.location.href,
+        "is_delete_after_ingest": true,
+    };
+    scrapeOrderData(transaction);
+    downloadJsonTransaction(transaction);
+    retitlePage(transaction);
+}
+
+function scrapeOrderData(transaction) {
+    console.log("scrapeOrderData")
+
+    getOrderMetaData(transaction);
+    getOrderItemization(transaction);
+}
+
+
+/*==========================================================================================
+ORDER METADATA
+==========================================================================================*/
+
+function getOrderMetaData(transaction) {
+    console.log("getOrderMetaData")
+
+    div_details_row = document.querySelector('[class*="InboxDetailsView-module---inbox-alerts-details-row---"').lastChild.firstChild.children
+    alert_header = div_details_row[2].innerText
+    // Process Dividend Alert
+    if (alert_header == "Dividend or interest paid") {
+        div_inbox_body = document.querySelector(".inbox-body-content").children
+
+        // Get Order Number
+        transaction["Order#"] = "dividend_payment_"
+
+        // Get OrderDate
+        date_str = div_details_row[0].innerText.replace(" ET", "")
+        processOrderDate(date_str, transaction)
+
+        // Get Order Total
+        transaction["Total"] = parsePrice(div_inbox_body[2].childNodes[6].textContent.trimLeft());
+
+        // Get Payment Methods(s) element
+        account = div_inbox_body[0].lastChild.textContent
+        transaction["PaymentMethod"] = account
+
+        // Description
+        description = div_inbox_body[1].innerText
+        security_header = div_inbox_body[2].childNodes[1].textContent
+        security = div_inbox_body[2].childNodes[2].textContent.trimEnd().replace("   ", " ")
+        security_ticker = security.replace(/.*\((.*)\)/, "$1")
+        transaction["Description"] = description + " " + security_header + security
+
+        transaction["Order#"] += security_ticker
+    }
+}
+
+/*==========================================================================================
+ORDER ITEMIZATION
+==========================================================================================*/
+
+function getOrderItemization(transaction){
+    console.log("getOrderItemization");
+
+    var purchased_items = [
+        [transaction["Description"], transaction["Total"]]
+    ];
+    transaction["Items"] = purchased_items;
+}
+
+
+processETradeInboxInvoice();
