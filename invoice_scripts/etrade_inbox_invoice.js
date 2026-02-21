2a70793d3f3d8440482fa99ffde1447c136e6284
jxramos <jaime.x.ramos@gmail.com>
2026-02-20 22:23:17 -0800

    implement eTrade funds transfers

--- invoice_scripts/etrade_inbox_invoice.js
+++ invoice_scripts/etrade_inbox_invoice.js
@@ -27,10 +27,11 @@ function getOrderMetaData(transaction) {
     console.log("getOrderMetaData")
 
     div_details_row = document.querySelector('[class*="InboxDetailsView-module---inbox-alerts-details-row---"').lastChild.firstChild.children
+    div_inbox_body = document.querySelector(".inbox-body-content").children
     alert_header = div_details_row[2].innerText
+
     // Process Dividend Alert
     if (alert_header == "Dividend or interest paid") {
-        div_inbox_body = document.querySelector(".inbox-body-content").children
 
         // Get Order Number
         transaction["Order#"] = "dividend_payment_"
@@ -54,6 +55,30 @@ function getOrderMetaData(transaction) {
         transaction["Description"] = description + " " + security_header + security
 
         transaction["Order#"] += security_ticker
+    } else if (alert_header == "Funds transfer confirmation") {
+        transfer_rows = div_inbox_body[1].firstElementChild.children
+
+        // Mark transaction as the transfer that it is
+        transaction["is_transfer"] = "account funds transfer"
+
+        // Get Order Number
+        order_id = transfer_rows[4].lastElementChild.textContent
+        transaction["Order#"] = order_id
+
+        // Get OrderDate
+        date_str = transfer_rows[3].lastElementChild.textContent
+        processOrderDate(date_str, transaction)
+
+        // Get Order Total
+        transaction["Total"] = parsePrice(transfer_rows[2].lastElementChild.textContent);
+
+        // Get Payment Methods(s) element
+        account_from = transfer_rows[0].lastElementChild.textContent
+        account_to   = transfer_rows[1].lastElementChild.textContent
+        transaction["Transfer"] = [account_from, account_to]
+
+        // Description
+        transaction["Description"] = "Confirmation: " + order_id + ", " + account_from + " --> " + account_to
     }
 }
 
@@ -63,6 +88,10 @@ ORDER ITEMIZATION
 
 function getOrderItemization(transaction){
     console.log("getOrderItemization");
+
+    if ("is_transfer" in transaction) {
+        return
+    }
 
     var purchased_items = [
         [transaction["Description"], transaction["Total"]]
