40a21289cc1a28f023d9af22d21c6dfcab272edb
jxramos <jaime.x.ramos@gmail.com>
2026-09-17 22:50:31 -0700

    eTrade inbox alerts parsing for multiple dividends scraper now iterates over inbox alerts with multiple dividend payouts. Also parses various inbox transaction types including * dividend or interest paid * funds transfer complete * funds transfer confirmation * funds transfer request received

--- invoice_scripts/etrade_inbox_invoice.js
+++ invoice_scripts/etrade_inbox_invoice.js
@@ -1,21 +1,21 @@
 function processETradeInboxInvoice() {
     console.log("processETradeInboxInvoice")
 
-    var transaction = {
-        "Vendor": "eTrade.com",
-        "URL": window.location.href,
-        "is_delete_after_ingest": true,
-    };
-    scrapeOrderData(transaction);
-    downloadJsonTransaction(transaction);
-    cleanupPage(transaction);
+    transactions = scrapeOrderData();
+    for (var i=0; i < transactions.length; i++){
+        downloadJsonTransaction(transactions[i]);
+    }
+    cleanupPage(transactions[0]);
 }
 
-function scrapeOrderData(transaction) {
+function scrapeOrderData() {
     console.log("scrapeOrderData")
 
-    getOrderMetaData(transaction);
-    getOrderItemization(transaction);
+    transactions = []
+    getOrderMetaData(transactions);
+    getOrderItemization(transactions);
+
+    return transactions
 }
 
 function cleanupPage(transaction) {
@@ -31,43 +31,65 @@ function cleanupPage(transaction) {
 }
 
 
+function createTransaction() {
+    var transaction = {
+        "Vendor": "eTrade.com",
+        "URL": window.location.href,
+        "is_delete_after_ingest": true,
+    };
+    return transaction
+}
+
 /*==========================================================================================
 ORDER METADATA
 ==========================================================================================*/
 
-function getOrderMetaData(transaction) {
+function getOrderMetaData(transactions) {
     console.log("getOrderMetaData")
 
     div_details_row = document.querySelector('[class*="InboxDetailsView-module---inbox-alerts-details-row---"').lastChild.firstChild.children
     div_inbox_body = document.querySelector(".inbox-body-content").children
-    alert_header = div_details_row[2].innerText
+    alert_header = div_details_row[2].innerText.toLowerCase()
+
+    // Get OrderDate
+    date_str = div_details_row[0].innerText.replace(" ET", "")
 
     // Process Dividend Alert
-    if (alert_header == "Dividend or interest paid") {
-
-        // Get Order Number
-        transaction["Order#"] = "dividend_payment_"
-
-        // Get OrderDate
-        date_str = div_details_row[0].innerText.replace(" ET", "")
-        processOrderDate(date_str, transaction)
-
-        // Get Order Total
-        transaction["Total"] = parsePrice(div_inbox_body[2].childNodes[6].textContent.trimLeft());
-
-        // Get Payment Methods(s) element
+    if (alert_header == "dividend or interest paid") {
+        // Get the account and security header
         account = div_inbox_body[0].lastChild.textContent
-        transaction["PaymentMethod"] = account
+        security_header = div_inbox_body[1].textContent
 
-        // Description
-        description = div_inbox_body[1].innerText
-        security_header = div_inbox_body[2].childNodes[1].textContent
-        security = div_inbox_body[2].childNodes[2].textContent.trimEnd().replace("   ", " ")
-        security_ticker = security.replace(/.*\((.*)\)/, "$1")
-        transaction["Description"] = description + " " + security_header + security
+        // detect multiple dividends paid on the same date
+        for (var i=0; i < div_inbox_body.length; i++){
+            if(! div_inbox_body[i].innerText.startsWith("Security: ")) {
+                continue
+            }
 
-        transaction["Order#"] += security_ticker
-    } else if (alert_header == "Funds transfer confirmation") {
+            var transaction = createTransaction()
+            processOrderDate(date_str, transaction)
+
+            // Get Order Number
+            transaction["Order#"] = "dividend_payment_"
+
+            // Get Order Total
+            transaction["Total"] = parsePrice(div_inbox_body[i].childNodes[6].textContent.trimLeft());
+
+            // Get Payment Methods(s) element
+            transaction["PaymentMethod"] = account
+
+            // Description
+            security = div_inbox_body[i].childNodes[2].textContent.trimEnd().replace("   ", " ")
+            security_ticker = security.replace(/.*\((.*)\)/, "$1")
+            description = div_inbox_body[i].childNodes[1].textContent + security
+            transaction["Description"] = security_header + " " + description
+
+            transaction["Order#"] += security_ticker
+
+            transactions.push(transaction)
+        }
+    } else if (alert_header == "funds transfer complete") {
+        var transaction = createTransaction()
         transfer_rows = div_inbox_body[1].firstElementChild.children
 
         // Get Order Number
@@ -75,7 +97,6 @@ function getOrderMetaData(transaction) {
         transaction["Order#"] = order_id
 
         // Get OrderDate
-        date_str = transfer_rows[3].lastElementChild.textContent
         processOrderDate(date_str, transaction)
 
         // Get Order Total
@@ -88,6 +109,57 @@ function getOrderMetaData(transaction) {
 
         // Description
         transaction["Description"] = "Confirmation: " + order_id + ", " + account_from + " --> " + account_to
+
+        transactions.push(transaction)
+    } else if (alert_header == "funds transfer confirmation") {
+        var transaction = createTransaction()
+        transfer_rows = div_inbox_body[0].childNodes
+
+        // Get Order Number
+        order_id = transfer_rows[6].textContent.replace(/.*\(reference #(.*)\).*/, "$1")
+        transaction["Order#"] = order_id
+
+        // Get OrderDate
+        processOrderDate(date_str, transaction)
+
+        // Get Order Total
+        transaction["Total"] = parsePrice(transfer_rows[1].textContent);
+
+        // Form the accounts transfer list
+        account_from = transfer_rows[3].textContent
+        account_to   = transfer_rows[5].textContent
+        transaction["Transfer"] = [account_from, account_to]
+
+        // Description
+        transaction["Description"] = "Confirmation: " + order_id + ", " + account_from + " --> " + account_to
+
+        transactions.push(transaction)
+    } else if (alert_header == "funds transfer request received") {
+        var transaction = createTransaction()
+        transfer_rows = div_inbox_body[1].firstElementChild.children
+
+        // Get Order Number
+        order_id = transfer_rows[3].lastElementChild.textContent
+        transaction["Order#"] = order_id
+
+        // Get OrderDate
+        processOrderDate(date_str, transaction)
+
+        // Get Order Total
+        transaction["Total"] = parsePrice(transfer_rows[1].textContent);
+
+        // Form the accounts transfer list
+        account_from = transfer_rows[0].innerText
+        account_to   = transfer_rows[1].innerText
+        transaction["Transfer"] = [account_from, account_to]
+
+        // Description
+        transaction["Description"] = "Confirmation: " + order_id + ", " + account_from + " --> " + account_to
+
+        transactions.push(transaction)
+    }
+    else {
+        console.warn("Running on unrecognized or not applicable alert page: " + alert_header)
     }
 }
 
@@ -95,17 +167,20 @@ function getOrderMetaData(transaction) {
 ORDER ITEMIZATION
 ==========================================================================================*/
 
-function getOrderItemization(transaction){
+function getOrderItemization(transactions){
     console.log("getOrderItemization");
 
-    if ("Transfer" in transaction) {
-        return
-    }
+    for (var i=0; i < transactions.length; i++){
+        transaction = transactions[i]
+        if ("Transfer" in transaction) {
+            continue
+        }
 
-    var purchased_items = [
-        [transaction["Description"], transaction["Total"]]
-    ];
-    transaction["Items"] = purchased_items;
+        var purchased_items = [
+            [transaction["Description"], transaction["Total"]]
+        ];
+        transaction["Items"] = purchased_items;
+    }
 }
 
 
